2b24efd49368bc0ffc39d103b19c3075d357f6cd
ihikki <798804385@qq.com>
2019-07-08 12:46:36 +0800

    更改接口参数

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -33,6 +33,6 @@ export default function createSong(musicData){
         album:musicData.albumname,
         duration:musicData.interval,
         image:`https://y.gtimg.cn/music/photo_new/T002R300x300M000${musicData.albummid}.jpg?max_age=2592000`,
-        url:`http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?guid=6878138000&vkey=8265149B6AF97D377774ACECA388A391CFF0A21B7A21ED9CE5457500F8C81A6D078E78AA0AAC84F0D7762A29AA8A30D95A8BF101C4130AD0&uin=0&fromtag=38`
+        url:`http://dl.stream.qqmusic.qq.com/C400${musicData.songmid}.m4a?guid=9400300656&vkey=1FE1565D83B3386C5FF7A4531F2D86361FCB1A6A2319DF2DB208788ACCD1939638E0C9D6420E3959A67F2E184F30733D394B49DC5F6876B8&uin=0&fromtag=38`
     })
 }
